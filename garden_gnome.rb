c685bac0ad76d00576c96f26c030797e9a468344
hannaertel <hannaertel@yahoo.com>
2018-06-21 20:55:26 +0000

    automatically backed up by learn

--- garden_gnome.rb
+++ garden_gnome.rb
@@ -19,7 +19,7 @@ class GardenGnome
  end
  end
  
-  gnome1 = GardenGnome.new("walter the worst")
+  gnome1 = GardenGnome.new("walter the worst", 4, "false", "blue")
   gnome2 = GardenGnome.new("James the Jerk", 3421)
   gnome3 = GardenGnome.new("alfred the abhorrent", 579, "true")
   
