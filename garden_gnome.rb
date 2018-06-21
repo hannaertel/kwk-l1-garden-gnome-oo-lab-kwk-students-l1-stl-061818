d05e431c1d0981c807421afe45ed047c3cf650d4
hannaertel <hannaertel@yahoo.com>
2018-06-21 20:33:47 +0000

    automatically backed up by learn

--- garden_gnome.rb
+++ garden_gnome.rb
@@ -1,7 +1,9 @@
 # Code your instances here
 class GardenGnome
   
-  attr_accessor :name, :age, :gluten_allergy, :personality, :hat_color, :gnaw, :shout, :introduce_self
+  attr_accessor :name, :age, :gluten_allergy, :hat_color
+  
+  attr_reader :personality, :gnaw, :shout, :introduce_self
   
  def initialize(name, age, gluten_allergy, personality, hat_color, gnaw, shout, introduce_self)
    @name = name
@@ -9,6 +11,7 @@ class GardenGnome
    @gluten_allergy = gluten_allergy
    @personality = evil
    @hat_color = 
+   @
    
   
   
